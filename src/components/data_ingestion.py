7770f6c733430096613bf855656fc729daf40ece
unknown <k10>
2022-10-04 13:14:25 +0530

    updates

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,5 @@
 from src.entity.config_entity import DataIngestionConfig
 from src.utils.storage_handler import S3Connector
-from tqdm import tqdm
 from from_root import from_root
 import splitfolders
 import os
@@ -9,8 +8,8 @@ import os
 class DataIngestion:
     def __init__(self):
         self.config = DataIngestionConfig()
-        self.connection = S3Connector()
-        self.client = self.connection.client
+        # self.connection = S3Connector()
+        # self.client = self.connection.client
 
     def download_dir(self):
         """
@@ -22,38 +21,41 @@ class DataIngestion:
 
         """
         try:
-            keys = []
-            dirs = []
-            next_token = ''
-            base_kwargs = {'Bucket': self.config.BUCKET, 'Prefix': self.config.PREFIX}
-            while next_token is not None:
-                kwargs = base_kwargs.copy()
-                if next_token != '':
-                    kwargs.update({'ContinuationToken': next_token})
-
-                results = self.client.list_objects_v2(**kwargs)
-                contents = results.get('Contents')
-                for i in contents:
-                    k = i.get('Key')
-                    if k[-1] != '/':
-                        keys.append(k)
-                    else:
-                        dirs.append(k)
-                next_token = results.get('NextContinuationToken')
+            # keys = []
+            # dirs = []
+            # next_token = ''
+            # base_kwargs = {'Bucket': self.config.BUCKET, 'Prefix': self.config.PREFIX}
+            # while next_token is not None:
+            #     kwargs = base_kwargs.copy()
+            #     if next_token != '':
+            #         kwargs.update({'ContinuationToken': next_token})
+            #
+            #     results = self.client.list_objects_v2(**kwargs)
+            #     contents = results.get('Contents')
+            #     for i in contents:
+            #         k = i.get('Key')
+            #         if k[-1] != '/':
+            #             keys.append(k)
+            #         else:
+            #             dirs.append(k)
+            #     next_token = results.get('NextContinuationToken')
 
             print("\n====================== Fetching Data ==============================\n")
 
-            for d in tqdm(dirs, desc="Creating Directories : "):
-                destination_path = os.path.join(self.config.RAW, d)
-                if not os.path.exists(os.path.dirname(destination_path)):
-                    os.makedirs(os.path.dirname(destination_path))
+            data_path = os.path.join(from_root(), self.config.RAW, self.config.PREFIX)
+            os.system(f"aws s3 sync s3://image-database-system/images/ {data_path}")
 
-            for k in tqdm(keys, desc="Downloading Images : "):
-                destination_path = os.path.join(self.config.RAW, k)
-                if not os.path.exists(os.path.dirname(destination_path)):
-                    os.makedirs(os.path.dirname(destination_path))
-                if not os.path.exists(destination_path):
-                    self.client.download_file(self.config.BUCKET, k, destination_path)
+            # for d in tqdm(dirs, desc="Creating Directories : "):
+            #     destination_path = os.path.join(self.config.RAW, d)
+            #     if not os.path.exists(os.path.dirname(destination_path)):
+            #         os.makedirs(os.path.dirname(destination_path))
+            #
+            # for k in tqdm(keys, desc="Downloading Images : "):
+            #     destination_path = os.path.join(self.config.RAW, k)
+            #     if not os.path.exists(os.path.dirname(destination_path)):
+            #         os.makedirs(os.path.dirname(destination_path))
+            #     if not os.path.exists(destination_path):
+            #         self.client.download_file(self.config.BUCKET, k, destination_path)
 
             print("\n====================== Fetching Completed ==========================\n")
 
@@ -83,11 +85,12 @@ class DataIngestion:
 
 
 if __name__ == "__main__":
-    paths = ["data", "data/raw", "data/splitted", "data/embeddings",
-             "model", "model/benchmark", "model/finetuned"]
+    paths = ["data", r"data\raw", r"data\splitted", r"data\embeddings",
+             "model", r"model\benchmark", r"model\finetuned"]
 
     for folder in paths:
         path = os.path.join(from_root(), folder)
+        print(path)
         if not os.path.exists(path):
             os.mkdir(folder)
 
